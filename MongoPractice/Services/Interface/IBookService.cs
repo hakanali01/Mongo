48de537b97e396e5da76c310cd0ca62b38bb6711
Hakan Ali Yasdı <hakanali91@gmail.com>
2020-12-09 10:53:29 +0300

    first commit

---
+++ MongoPractice/Services/Interface/IBookService.cs
@@ -0,0 +1,23 @@
+﻿using MongoPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoPractice.Services
+{
+    interface IBookService 
+    {
+        public List<Book> Get();
+
+        public Book Get(string id);
+
+        public Book Create(Book book);
+
+        public void Update(string id, Book bookIn);
+
+        public void Remove(Book bookIn);
+
+        public void Remove(string id);
+    }
+}
